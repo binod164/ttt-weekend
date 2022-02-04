d67ddf23f78afdbd62cce9b824c3469b4da5bad4
binod164 <binod7_grg@hotmail.com>
2022-02-04 11:55:32 -0600

    Add init function

--- js/app.js
+++ js/app.js
@@ -3,16 +3,48 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let boardArray, turn, winner;
+
 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squares = document.querySelectorAll(".sq")
+// console.log(squares)
 
+const gameStatus = document.querySelectorAll("#game-message")
+// console.log(gameStatus)
+
+// let reset = document.querySelector(".reset")
+// console.log(reset)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+// squares.addEventListener("click",init)
+
+// squares.addEventListener("click",function(evt){
+// squares.innerHTML = "X"
+// })
+
+// reset.addEventListener("click",init){
+// squares.innerHTML = ""
+// }
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+// console.log(init())
 
+function init(){
+  boardArray = [null,null,null,null,null,null,null,null,null]
+  playerX = 1;
+  playerO = -1;
+  winner = null;
+  render();
+}
+
+function render(){
+  boardArray.forEach(function(square){
+
+  })
